82240db31ec1bce19670c4bc4edb939dd902eebd
Osric Wilkinson <gitmoose@fluffypeople.com>
2019-07-14 07:05:32 +0100

    Update image when the machine wakes up

--- EarthDownloader.cs
+++ EarthDownloader.cs
@@ -12,34 +12,76 @@ using System.Threading.Tasks;
 
 namespace EarthBackdrop {
     class EarthDownloader {
-        private static readonly HttpClient client = new HttpClient();
-        private const string TARGET_URL = "https://eumetview.eumetsat.int/static-images/latestImages/EUMETSAT_MSG_RGBNatColourEnhncd_FullResolution.jpg";
-        private bool running = false;
         private const int SPI_SETDESKWALLPAPER = 20;
         private const int SPIF_UPDATEINIFILE = 0x01;
         private const int SPIF_SENDWININICHANGE = 0x02;
 
+        // TODO: Load this from some kind of config repository
+        private const string TARGET_URL = "https://eumetview.eumetsat.int/static-images/latestImages/EUMETSAT_MSG_RGBNatColourEnhncd_FullResolution.jpg";
+        private static readonly HttpClient client = new HttpClient();
+
+        private readonly object Lock = new object();
+        private bool running = false;
+        private EarthBackdropApplicationContext earthBackdropApplicationContext;
+
+        public EarthDownloader(EarthBackdropApplicationContext earthBackdropApplicationContext) {
+            this.earthBackdropApplicationContext = earthBackdropApplicationContext;
+        }
+
         [DllImport("user32.dll", CharSet = CharSet.Auto)]
         static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
 
+        /// <summary>
+        /// Start a new download thread (if we're not already running)
+        /// </summary>
         internal void Start() {
+            lock (Lock) {
+                if (running) {
+                    return;
+                }
+                running = true;
+                new Thread(() => {
+                    Thread.CurrentThread.IsBackground = true;
+                     Run();
+                }).Start();
 
-            running = true;
-            new Thread(async () => {
-                Thread.CurrentThread.IsBackground = true;
-                await Run();
-            }).Start();
-        }
-        
-
-        private async Task Run() {
-            while (running) {
-                var img = await DownloadImage();
-                SetBackground(img);
-                Sleep();
             }
         }
 
+        /// <summary>
+        /// Wake up the download thread
+        /// </summary>
+        internal void Refresh() {
+            lock(Lock) {
+                Monitor.Pulse(Lock);
+            }
+        }
+
+        /// <summary>
+        /// Download the image, then wait for an hour (or untill we're woken up again)
+        /// </summary>
+        private void Run() {
+            while (true) {
+                lock (Lock) {
+                    if (!running) {
+                        return;
+                    }
+                    var img = DownloadImage();
+                    if (img != null) {
+                        Image earth = img.Result;
+                        string msg = String.Format("Last download was at {0}", DateTime.Now.ToString("f"));
+                        SetBackground(earth);
+                        earthBackdropApplicationContext.UpdateTrayIcon(earth, msg);
+                    }
+                    Monitor.Wait(Lock, TimeSpan.FromHours(1));
+                }          
+            }
+        }
+
+        /// <summary>
+        /// Fetch the image from the intranet
+        /// </summary>
+        /// <returns>A download task</returns>
         private async Task<Image> DownloadImage() {
             HttpResponseMessage resp = await client.GetAsync(TARGET_URL);
             if (resp.IsSuccessStatusCode) {
@@ -48,6 +90,11 @@ namespace EarthBackdrop {
             return null;
         }
 
+        /// <summary>
+        /// Set the desktop background. The image passed is saved as a bitmap
+        /// to a temporary path (from Path.GetTempPath()) first.
+        /// </summary>
+        /// <param name="img">The image data to use for the background</param>
         private void SetBackground(Image img) {
             string tempPath = Path.Combine(Path.GetTempPath(), "wallpaper.bmp");
             img.Save(tempPath, System.Drawing.Imaging.ImageFormat.Bmp);
@@ -60,11 +107,16 @@ namespace EarthBackdrop {
                 SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
         }
 
-        private void Sleep() {
-            Thread.Sleep(TimeSpan.FromMinutes(60));
-        }
+        /// <summary>
+        /// Tells the download thread to stop (if it's running)
+        /// </summary>
         internal void Stop() {
-            running = false;
+            lock (Lock) {
+                if (running) {
+                    running = false;
+                    Monitor.Pulse(Lock);
+                }
+            }
         }
     }
 }
---
+++ Startup.cs
@@ -0,0 +1,100 @@
+﻿using EarthBackdrop.Properties;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EarthBackdrop {
+    static class Startup {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main() {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new EarthBackdropApplicationContext());
+        }
+    }
+
+    public class EarthBackdropApplicationContext : ApplicationContext {
+        private readonly NotifyIcon trayIcon;
+        private readonly EarthDownloader downloader;
+
+        public EarthBackdropApplicationContext() {
+
+            trayIcon = new NotifyIcon() {
+                Icon = Icon.FromHandle(Resources.AppIcon.GetHicon()),
+                ContextMenu = new ContextMenu(new MenuItem[] {
+                    new MenuItem("Exit", Exit)
+                }),
+                Visible = true
+            };
+
+            downloader = new EarthDownloader(this);
+            downloader.Start();
+
+            SystemEvents.PowerModeChanged += OnPowerModeChanged; 
+        }
+
+        internal void UpdateTrayIcon(Image source,string msg){
+            trayIcon.Icon = IconFromImage(source);
+            trayIcon.Text = msg;
+        }
+
+
+        // Copied from https://stackoverflow.com/a/21389253/195833
+        private static Icon IconFromImage(Image img) {
+            var ms = new System.IO.MemoryStream();
+            var bw = new System.IO.BinaryWriter(ms);
+            // Header
+            bw.Write((short)0);   // 0 : reserved
+            bw.Write((short)1);   // 2 : 1=ico, 2=cur
+            bw.Write((short)1);   // 4 : number of images
+                                  // Image directory
+            var w = img.Width;
+            if (w >= 256) w = 0;
+            bw.Write((byte)w);    // 0 : width of image
+            var h = img.Height;
+            if (h >= 256) h = 0;
+            bw.Write((byte)h);    // 1 : height of image
+            bw.Write((byte)0);    // 2 : number of colors in palette
+            bw.Write((byte)0);    // 3 : reserved
+            bw.Write((short)0);   // 4 : number of color planes
+            bw.Write((short)0);   // 6 : bits per pixel
+            var sizeHere = ms.Position;
+            bw.Write((int)0);     // 8 : image size
+            var start = (int)ms.Position + 4;
+            bw.Write(start);      // 12: offset of image data
+                                  // Image data
+            img.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+            var imageSize = (int)ms.Position - start;
+            ms.Seek(sizeHere, System.IO.SeekOrigin.Begin);
+            bw.Write(imageSize);
+            ms.Seek(0, System.IO.SeekOrigin.Begin);
+
+            // And load it
+            return new Icon(ms);
+        }
+
+        private void OnPowerModeChanged(object sender, PowerModeChangedEventArgs e) {
+            switch (e.Mode) {
+                case PowerModes.Resume:
+                    downloader.Refresh();
+                    break;
+            }
+        }
+
+        void Exit(object sender, EventArgs e) {
+            SystemEvents.PowerModeChanged -= OnPowerModeChanged;
+
+            downloader.Stop();
+            trayIcon.Visible = false;
+            Application.Exit();
+        }
+
+    }
+}
